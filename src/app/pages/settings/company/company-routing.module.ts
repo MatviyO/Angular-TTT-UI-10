8d6c244e3c5e858b4d22eb5052baaca5a23eced9
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-10-08 18:06:55 +0300

    gdfgdf

---
+++ src/app/pages/settings/company/company-routing.module.ts
@@ -0,0 +1,20 @@
+
+import {NgModule} from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import {CompanyComponent} from './company.component';
+import {CompanyListComponent} from './components/list/company-list.component';
+
+const routes: Routes = [
+  {path: 'company', component: CompanyComponent, children: [
+      {path: 'list', component: CompanyListComponent },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class CompanyRoutingModule {
+}
--- src/app/pages/settings/company/company.module.ts
+++ src/app/pages/settings/company/company.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CompanyComponent} from './company.component';
+import { CompanyListComponent } from './components/list/company-list.component';
+import {CompanyRoutingModule} from './company-routing.module';
+import {NgaModule} from '../../../theme/nga.module';
+import {FormsModule} from '@angular/forms';
+import {NguiAutoCompleteModule} from '@ngui/auto-complete';
 
 @NgModule({
-  declarations: [CompanyComponent],
+  declarations: [CompanyComponent, CompanyListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CompanyRoutingModule,
+    NgaModule,
+    FormsModule,
+    NguiAutoCompleteModule,
   ],
   providers: []
 })
