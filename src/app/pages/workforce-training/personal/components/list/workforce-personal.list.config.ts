87dd66be5bb54f6bde486428bcd97421d128a02d
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-10 17:08:44 +0200

    sss

--- src/app/pages/workforce-training/personal/components/list/workforce-personal.list.config.ts
+++ src/app/pages/workforce-training/personal/components/list/workforce-personal.list.config.ts
@@ -7,10 +7,10 @@ import {TriggerService, WorkforceTrainingPersonalService} from '../../../../../c
 @Injectable()
 export class WorkforcePersonalListConfig implements IListWithTriggersConfig<WorkforceTrainingPersonal> {
     triggerType = '';
-    componentTitle = 'Workforce personal';
-    includes = 'Items,EmploymentRecord.Application,EmploymentRecord.CompanyTrade';
-    selectJSONPath = 'maxHours;items[*].totalHours;items[*].month;items[*].maxHours;employmentRecordId;employmentRecord.application.firstName;employmentRecord.application.lastName;employmentRecord.application.isActive';
-    constructor(
+  componentTitle = 'Workforce personal';
+  includes = 'Items,EmploymentRecord.Application,EmploymentRecord.CompanyTrade';
+  selectJSONPath = 'maxHours;items[*].totalHours;items[*].month;items[*].maxHours;employmentRecordId;employmentRecord.application.firstName;employmentRecord.application.lastName;employmentRecord.application.isActive';
+  constructor(
         @Inject(WorkforceTrainingPersonalService) public dataSvc: IDataService<WorkforceTrainingPersonal>,
         @Inject(TriggerService) public triggersSvc: ITriggerService,
         public injector: Injector,
